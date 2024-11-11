1ee182b3bca7683c546ccbbdd7511ee973aefd5d
Abhishek Bhardwaj <bhardwajabhi2701@gmail.com>
2024-11-11 08:36:00 +0530

    Implemented backend for signup, login and logout via JWT

---
+++ backend/controllers/authController.js
@@ -0,0 +1,42 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN | "1d" });
+}
+
+exports.signup = async (req, res) => {
+    const { firstName, lastName, username, email, password } = req.body;
+
+    try {
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = await User.create({ firstName, lastName, username, email, password });
+        const token = generateToken(user._id);
+        res.status(201).json({ token, message: "User created successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(await user.matchPassword(password))) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const token = generateToken(user._id);
+        res.status(200).json({ token, message: "Login successful" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+exports.logout = async (req, res) => {
+    res.status(200).json({ message: "Logout successful" });
+}
